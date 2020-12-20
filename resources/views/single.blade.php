ce5c2977b095bba623abd5a1618c297bdc3306b0
mostafamedht98 <mostafamedht98"gmail>
2020-12-20 16:21:57 +0200

    make disques

--- resources/views/single.blade.php
+++ resources/views/single.blade.php
@@ -166,19 +166,18 @@
                                     <span class="long-line"></span>
                                 </div>
                             </div>
-                        </div>
-        
-                        <div class="row">
-        
-                        </div>
-        
+
+                            @include('inc.disqus')
+
+                        </div>   
         
                     </div>
         
                     <!-- End Post Details -->
         
                     <!-- Sidebar-->
-        
+                    <br>
+                    <br>
                     <div class="col-lg-12">
                         <aside aria-label="sidebar" class="sidebar sidebar-right">
                             <div  class="widget w-tags">
@@ -191,15 +190,9 @@
                                 </div>
         
                                 <div class="tags-wrap">
-                                    <a href="#" class="w-tags-item">SEO</a>
-                                    <a href="#" class="w-tags-item">Advertising</a>
-                                    <a href="#" class="w-tags-item">Business</a>
-                                    <a href="#" class="w-tags-item">Optimization</a>
-                                    <a href="#" class="w-tags-item">Digital Marketing</a>
-                                    <a href="#" class="w-tags-item">Social</a>
-                                    <a href="#" class="w-tags-item">Keyword</a>
-                                    <a href="#" class="w-tags-item">Strategy</a>
-                                    <a href="#" class="w-tags-item">Audience</a>
+                                    @foreach ($tags as $tag)
+                                        <a href="#" class="w-tags-item">SEO</a>
+                                    @endforeach
                                 </div>
                             </div>
                         </aside>
